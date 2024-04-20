7c3418293620a3483cec747a13c6ebafb84567ae
Danny Mor <yits.dannymor@gmail.com>
2024-04-20 03:18:55 +0300

    burger

--- js/burger.js
+++ js/burger.js
@@ -2,11 +2,11 @@ const ACTIVE_CLASS = "active";
 const OPEN_CLASS = "open";
 
 export const initBurger = () => {
-  const burgerBtn = document.querySelector(".navList__burger");
-  const burgerMenu = document.querySelector(".navList__wrap-item");
+  const burgerBtn = document.querySelector(".location__burger");
+  const burgerMenu = document.querySelector(".location__wrap-menu");
 
   burgerBtn.addEventListener("click", () => {
     burgerMenu.classList.toggle(ACTIVE_CLASS);
-    document.body.classList.toggle(OPEN_CLASS);
+    // document.body.classList.toggle(OPEN_CLASS);
   });
 };
