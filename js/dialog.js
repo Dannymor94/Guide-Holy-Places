31e54735940642754d5fe9be0e9c80e7185c8c9e
Danny Morozov <yits.dannymor@gmail.com>
2024-05-27 22:05:27 +0300

    menu

--- js/dialog.js
+++ js/dialog.js
@@ -1,15 +1,36 @@
-  export const dialog = ()=>{
+//   export const dialog = ()=>{
 
-  const open = document.getElementById('open');
-  const close = document.getElementById('close');
-  const dialog =  document.getElementById('dialog');
+//   const open = document.getElementById('open');
+//   const close = document.getElementById('close');
+//   const dialog =  document.getElementById('dialog');
   
-  open.addEventListener('click', ()=>{
+//   open.addEventListener('click', ()=>{
     
-    dialog.showModal();
+//     dialog.showModal();
+//   })
+//   close.addEventListener('click', ()=>{
+//     dialog.close();
+//   })
+// };
+// dialog();
+
+ export const closeItem = () =>{
+  const inner = document.getElementById('header-inner');
+  const cross = document.getElementById('header-cross');
+  const HeaderText = document.getElementById('HeaderTextPop');
+  
+  const icon = document.getElementsByClassName('nav__logo');
+  Array.from(icon).forEach(icon => {
+    icon.addEventListener('click',()=>{
+      HeaderText.classList.toggle('active');
+    })
+  });
+  inner.addEventListener('click', ()=>{
+    inner.style.display = 'none';
   })
-  close.addEventListener('click', ()=>{
-    dialog.close();
-  })
+  cross.addEventListener('click' , ()=>{
+  inner.style.display = 'none';
+})
 };
-dialog();
+closeItem();
+
